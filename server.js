ec83f98cedb909d43f26f432f000dc477c03901a
Fraz Khan <fraz9838@gmail.com>
2022-11-28 06:14:59 +0500

    7

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
 const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV === "production") {
-  app.use("client/build");
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
   });
